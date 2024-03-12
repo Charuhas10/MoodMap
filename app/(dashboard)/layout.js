e07d8090e5443884109e54eddf612fff558bf373
Charuhas Reddy <charuhasreddybalam@gmail.com>
2024-03-12 16:55:40 +0530

    Added DB

---
+++ app/(dashboard)/layout.js
@@ -0,0 +1,36 @@
+import { UserButton } from "@clerk/nextjs";
+import Link from "next/link";
+import styles from "./dashboard.module.css";
+
+const links = [
+  { href: "/", label: "Home" },
+  { href: "/journal", label: "Journal" },
+  { href: "/history", label: "History" },
+];
+
+const DashboardLayout = ({ children }) => {
+  return (
+    <div className=" relative w-screen h-screen">
+      <aside className=" absolute top-0 left-0 bottom-0 h-full w-[200px] border-r border-color">
+        <div>MOODMAP</div>
+        <ul>
+          {links.map((link) => (
+            <li key={link.href} className=" py-6 px-2">
+              <Link href={link.href}>{link.label}</Link>
+            </li>
+          ))}
+        </ul>
+      </aside>
+      <div className=" ml-[200px] h-full">
+        <header className=" h-[60px] border-b border-black/10">
+          <div className=" h-full w-full px-6 flex items-center justify-end">
+            <UserButton />
+          </div>
+        </header>
+        <div className={styles.children}>{children}</div>
+      </div>
+    </div>
+  );
+};
+
+export default DashboardLayout;
